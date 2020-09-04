ced77961a73353d106b7752ecf70c434c4c34d2e
Krish <vamsikrish.49@gmail.com>
2020-09-04 21:13:48 +0530

    Day 4 solution

---
+++ Day 4/Solution.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/partition-labels/
+
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> result = new ArrayList<>();
+        
+        int[] last_indices = new int[26];
+        for(int i=0; i<S.length(); i++) {
+            last_indices[S.charAt(i) - 'a'] = i;
+        }
+        
+        int start = 0, end = 0;
+        for(int i=0; i<S.length(); i++) {
+            end = Math.max(end,last_indices[S.charAt(i) - 'a']);
+            if(i == end) {
+                result.add(end-start+1);
+                start = end + 1;
+            }
+        }
+        return result;
+    }
+}
