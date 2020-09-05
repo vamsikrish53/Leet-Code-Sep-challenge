89968e04b5937a957e87b8941ede9e9a02b951fd
Krish <vamsikrish.49@gmail.com>
2020-09-05 21:10:38 +0530

    Day 5 solution

---
+++ Day 5/Solution.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root1, ans);
+        inorder(root2, ans);
+        Collections.sort(ans); // Here it calls List.sort(object) method.
+        return ans;
+    }
+    private void inorder(TreeNode n, List<Integer> ans) {
+        if (n == null) return;
+        inorder(n.left, ans);
+        ans.add(n.val);
+        inorder(n.right, ans);
+    }
+}
